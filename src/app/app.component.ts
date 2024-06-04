7ecbe939fab638bc5648652edab788903ca9e4e3
SahenJavac <sahen92developer@gmail.com>
2024-06-05 01:36:35 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'MyDebugger';
+
+   printName(firstName: string, lastName: string): string {
+    console.log(`${firstName} ${lastName}`);
+    return `${firstName} ${lastName}`;
+  }
+  
+   printNTimes(n: number, value: string): void {
+    for (let i = 0; i < n; i++) {
+      console.log(value);
+    }
+  }
+  
+ 
+  
+  // Providing example arguments for function calls to avoid errors
+
+  ngOnInit(){
+    this.printNTimes(32,"Hello")
+    this.printName('Sahen','Liyanage')  
+
+  }
 }
